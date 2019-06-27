a593aab9f7a7e8c1af58616762c4514e3355b052
yepterence <terence.yep@gmail.com>
2019-06-27 19:01:34 -0400

    Added two instances of 4wheelrobot and renamed them Marco-I and Polo-I. Modified hello.cpp with different print message. Created building using building editor, and created world with building and robots

--- myrobot/script/hello.cpp
+++ myrobot/script/hello.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Hello World!\n");
+              printf("Welcome to the factory floor!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
